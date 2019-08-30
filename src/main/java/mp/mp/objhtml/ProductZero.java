8cfa9c303fdfb855233d3835ef85b3208c03b3aa
germansalinas700 <53483873+germansalinas700@users.noreply.github.com>
2019-08-30 15:29:38 -0400

    Update ProductZero.java

--- src/main/java/mp/mp/objhtml/ProductZero.java
+++ src/main/java/mp/mp/objhtml/ProductZero.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.WebElement;
 
 public class ProductZero extends PageObject {
 	
-	private WebElement productzero = driver.findElement(By.cssSelector(".product-layout:nth-child(2) .img-responsive"));
-/html[1]/body[1]/div[2]/div[1]/div[1]/div[3]/div[3]/div[1]/div[1]/a[1]/img[1]
+	private WebElement productzero = driver.findElement(By.xpath("(/html[1]/body[1]/div[2]/div[1]/div[1]/div[3]/div[3]/div[1]/div[1]/a[1]/img[1])"));
+
 	public ProductZero(WebDriver driver) {
 		super(driver);
 	}
